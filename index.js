a1ddc12c9cc0e5d24d4bf6de49059f21e3632a27
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-07-16 16:11:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(array) {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(array) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+
